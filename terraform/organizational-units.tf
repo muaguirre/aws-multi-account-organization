00c7c623871e47e00832d8727f6f6badd6e2edde
muaguirre <106277535+muaguirre@users.noreply.github.com>
2026-07-07 04:23:45 +0200

    Add Terraform organizational units

---
+++ terraform/organizational-units.tf
@@ -0,0 +1,17 @@
+# Organizational Units
+# These OUs separate security, infrastructure and workload accounts.
+
+resource "aws_organizations_organizational_unit" "security" {
+  name      = "Security"
+  parent_id = aws_organizations_organization.main.roots[0].id
+}
+
+resource "aws_organizations_organizational_unit" "infrastructure" {
+  name      = "Infrastructure"
+  parent_id = aws_organizations_organization.main.roots[0].id
+}
+
+resource "aws_organizations_organizational_unit" "workloads" {
+  name      = "Workloads"
+  parent_id = aws_organizations_organization.main.roots[0].id
+}
